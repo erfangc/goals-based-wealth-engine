dc45e22ac9a4af82cd5bd49b936a9d73b8115126
erfangchen <erfangc@gmail.com>
2020-03-05 14:23:29 -0500

    added the French-Fama factor levels

--- src/main/kotlin/io/github/erfangc/assets/AssetTimeSeriesService.kt
+++ src/main/kotlin/io/github/erfangc/assets/AssetTimeSeriesService.kt
@@ -3,6 +3,7 @@ package io.github.erfangc.assets
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.streams.toList
 
 @Service
@@ -14,9 +15,9 @@ class AssetTimeSeriesService {
      *
      * The caller can use `groupBy` operations to separate time series by asset post call
      */
-    fun getReturnTimeSeries(assetIds: List<String>,
-                            start: LocalDate,
-                            stop: LocalDate): List<TimeSeriesDatum> {
+    fun getDailyReturnTimeSeries(assetIds: List<String>,
+                                 start: LocalDate,
+                                 stop: LocalDate): List<TimeSeriesDatum> {
         val returnTimeSeries = assetIds.map { assetId ->
 
             val reader = ClassPathResource("assets/time-series/$assetId.json")
@@ -57,6 +58,41 @@ class AssetTimeSeriesService {
                         // we find the return time series datum or else return 0.0
                         returnTimeSeries[assetId]?.get(date)
                                 ?: TimeSeriesDatum(assetId = assetId, date = date, field = Field.RETURN, value = 0.0)
+                    }
+                }
+    }
+
+    /**
+     * Computes the monthly rather than daily return of a given list of assets
+     * and return them in a single (co-mingled) list of time series datum objects
+     *
+     * The caller can use `groupBy` operations to separate time series by asset post call
+     *
+     * Monthly returns are usually used to find expected returns as the factor returns are usually computed
+     * at this interval
+     */
+    fun getMonthlyReturnTimeSeries(assetIds: List<String>,
+                                   start: LocalDate,
+                                   stop: LocalDate): List<TimeSeriesDatum> {
+        // first find the daily returns then take geometric averages
+        return getDailyReturnTimeSeries(assetIds, start, stop)
+                // first group by assetId, within each asset's daily return history group by again by year/month
+                .groupBy { it.assetId }
+                .flatMap { (assetId, v) ->
+                    v.groupBy {
+                        it.date.format(DateTimeFormatter.ofPattern("yyyy-MM"))
+                    }.map {
+                        (date, dailyReturns) ->
+                        val value = dailyReturns.fold(1.0) {
+                            acc: Double, datum: TimeSeriesDatum ->
+                            acc * (datum.value + 1.0)
+                        } - 1.0
+                        TimeSeriesDatum(
+                                assetId = assetId,
+                                value = value,
+                                field = Field.RETURN,
+                                date = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM"))
+                        )
                     }
                 }
     }
