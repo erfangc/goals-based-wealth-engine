4b4271a2be970c250ce04e27efbec41cb5d13e81
erfangchen <erfangc@gmail.com>
2020-03-06 21:02:06 -0500

    added outer shells for the convex optimizer and variance / expected value terms

--- src/main/kotlin/io/github/erfangc/convexoptimizer/ConvexOptimizerService.kt
+++ src/main/kotlin/io/github/erfangc/convexoptimizer/ConvexOptimizerService.kt
@@ -1,7 +1,10 @@
 package io.github.erfangc.convexoptimizer
 
+import ilog.concert.IloNumExpr
+import ilog.concert.IloNumVar
 import ilog.concert.IloObjectiveSense
 import ilog.cplex.IloCplex
+import io.github.erfangc.assets.Asset
 import io.github.erfangc.assets.AssetService
 import io.github.erfangc.covariance.CovarianceService
 import io.github.erfangc.expectedreturns.ExpectedReturnsService
@@ -16,6 +19,15 @@ class ConvexOptimizerService(
         private val userService: UserService
 ) {
 
+    internal class OptimizationContext(
+            val cplex: IloCplex,
+            val req: OptimizePortfolioRequest,
+            val assets: Map<String, Asset>,
+            val assetIds: List<String>,
+            val assetVars: Map<String, IloNumVar>,
+            val expectedReturns: Map<String, Double>
+    )
+
     /**
      * For convex optimization there are two classes of objectives:
      *
@@ -29,22 +41,71 @@ class ConvexOptimizerService(
      *
      */
     fun optimizePortfolio(req: OptimizePortfolioRequest): OptimizePortfolioResponse {
-        val assetIds = assetIds(req)
-        val assets = assetService.getAssets(assetIds).associateBy { it.assetId }
 
-        val cplex = IloCplex()
+        val ctx = optimizationContext(req)
+        val portfolios = req.portfolios?.map {
+            portfolioDefinition -> portfolioDefinition.portfolio
+            // figure out how to create position trading variables that must tie back to the asset variables
+        }
 
-        //
-        // build the risk terms on which we minimize
-        //
-        val response = covarianceService.computeCovariances(assetIds)
+        val cplex = ctx.cplex
+        cplex.objective(IloObjectiveSense.Minimize, varianceExpr(ctx))
+        // total weight must be 100%
+        cplex.add(cplex.eq(1.0, cplex.sum(ctx.assetVars.values.toTypedArray())))
+        // the resulting portfolio must target the level of return
+        cplex.add(cplex.eq(returnExpr(ctx), req.objectives.expectedReturn))
 
-        cplex.objective(IloObjectiveSense.Maximize, cplex.numVar(1.0,1.0))
+        val solved = cplex.solve()
+        if (!solved) {
+            throw RuntimeException("The convex optimization problem cannot be solved")
+        }
 
         //
         // position level restrictions (tax etc.)
         //
         TODO()
+    }
+
+    private fun varianceExpr(ctx: OptimizationContext): IloNumExpr? {
+        val cplex = ctx.cplex
+        //
+        // build the risk terms on which we minimize
+        //
+        val response = covarianceService.computeCovariances(ctx.assetIds)
+        val covariances = response.covariances
+        val assetIndexLookup = response.assetIndexLookup
+        val varianceTerms = ctx.assetIds.flatMap { a1 ->
+            ctx.assetIds.map { a2 ->
+                val a1Idx = assetIndexLookup[a1] ?: error("")
+                val a2Idx = assetIndexLookup[a2] ?: error("")
+                val covariance = covariances[a1Idx][a2Idx]
+                val a1Var = ctx.assetVars[a1] ?: error("")
+                val a2Var = ctx.assetVars[a2] ?: error("")
+                cplex.prod(a1Var, a2Var, covariance)
+            }
+        }.toTypedArray()
+        return cplex.sum(varianceTerms)
+    }
+
+    private fun returnExpr(ctx: OptimizationContext): IloNumExpr? {
+        val cplex = ctx.cplex
+        val returnExpr = ctx.assetIds.map { assetId ->
+            val assetVar = ctx.assetVars[assetId] ?: error("")
+            val expectedReturn = ctx.expectedReturns[assetId] ?: error("")
+            cplex.prod(assetVar, expectedReturn)
+        }.toTypedArray()
+        return cplex.sum(returnExpr)
+    }
+
+    private fun optimizationContext(req: OptimizePortfolioRequest, cplex: IloCplex = IloCplex()): OptimizationContext {
+        val assetIds = assetIds(req)
+        val assets = assetService.getAssets(assetIds).associateBy { it.assetId }
+
+        // each asset is an decision variable
+        val assetVars = assetIds.map { assetId -> assetId to cplex.numVar(0.0, 1.0, assetId) }.toMap()
+        val expectedReturns = expectedReturnsService.getExpectedReturns(assetIds)
+
+        return OptimizationContext(cplex, req, assets, assetIds, assetVars, expectedReturns)
     }
 
     private fun assetIds(req: OptimizePortfolioRequest): List<String> {
