fa3ec86a6639a778492b9d17d9d8b20aea184750
erfangchen <erfangc@gmail.com>
2020-03-08 16:45:08 -0400

    implemented analysis service

--- src/main/kotlin/io/github/erfangc/analysis/AnalysisService.kt
+++ src/main/kotlin/io/github/erfangc/analysis/AnalysisService.kt
@@ -2,8 +2,13 @@ package io.github.erfangc.analysis
 
 import io.github.erfangc.covariance.CovarianceService
 import io.github.erfangc.expectedreturns.ExpectedReturnsService
+import io.github.erfangc.marketvalueanalysis.MarketValueAnalysis
+import io.github.erfangc.marketvalueanalysis.MarketValueAnalysisRequest
 import io.github.erfangc.marketvalueanalysis.MarketValueAnalysisService
+import io.github.erfangc.portfolios.Portfolio
+import io.github.erfangc.portfolios.Position
 import org.springframework.stereotype.Service
+import kotlin.math.sqrt
 
 @Service
 class AnalysisService(
@@ -11,7 +16,65 @@ class AnalysisService(
         private val expectedReturnsService: ExpectedReturnsService,
         private val covarianceService: CovarianceService
 ) {
+
     fun analyze(req: AnalysisRequest): AnalysisResponse {
-        TODO()
+        // flatten the group of portfolios into a single one
+        val positions = mergePositions(req)
+        val marketValueAnalysis = marketValueAnalysis(positions)
+
+        // compute variance
+        val assetIds = positions.map { it.assetId }.distinct()
+        val expectedReturn = expectedReturn(positions, assetIds, marketValueAnalysis)
+        val volatility = volatility(assetIds, positions, marketValueAnalysis)
+
+        return AnalysisResponse(marketValueAnalysis, expectedReturn = expectedReturn, volatility = volatility)
     }
+
+    private fun volatility(assetIds: List<String>, positions: List<Position>, marketValueAnalysis: MarketValueAnalysis): Double {
+        val (covariances, assetIndexLookup) = covarianceService.computeCovariances(assetIds)
+        val variance = positions.flatMap { p1 ->
+            val a1 = assetIndexLookup[p1.assetId] ?: error("")
+            positions.map { p2 ->
+                val a2 = assetIndexLookup[p2.assetId] ?: error("")
+                val w1 = marketValueAnalysis.weights[p1.assetId] ?: error("")
+                val w2 = marketValueAnalysis.weights[p2.assetId] ?: error("")
+                w1 * w2 * covariances[a1][a2]
+            }
+        }.sum()
+        return sqrt(variance)
+    }
+
+    private fun expectedReturn(positions: List<Position>,
+                               assetIds: List<String>,
+                               marketValueAnalysis: MarketValueAnalysis): Double {
+        // compute expected returns
+        val expectedReturns = expectedReturnsService.getExpectedReturns(assetIds)
+        return positions.sumByDouble { position ->
+            val er = expectedReturns[position.assetId] ?: 0.0
+            val wt = marketValueAnalysis.weights[position.id] ?: 0.0
+            er * wt
+        }
+    }
+
+    private fun mergePositions(req: AnalysisRequest): List<Position> {
+        return req
+                .portfolios
+                .flatMap { it.positions }
+                .groupBy { it.assetId }.map { (assetId, positions) ->
+                    val quantity = positions.sumByDouble { it.quantity }
+                    Position(
+                            id = assetId,
+                            assetId = assetId,
+                            quantity = quantity
+                    )
+                }
+    }
+
+    private fun marketValueAnalysis(positions: List<Position>): MarketValueAnalysis {
+        return marketValueAnalysisService.analyze(MarketValueAnalysisRequest(Portfolio(
+                id = "",
+                positions = positions
+        )))
+    }
+
 }
