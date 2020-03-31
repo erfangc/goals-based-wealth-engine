b402c5c81ba3d54265bbff15a99eb84beaac3c4f
erfangchen <erfangc@gmail.com>
2020-03-31 13:29:49 -0400

    modified SearchAssetController.kt

--- src/main/kotlin/io/github/erfangc/assets/SearchAssetController.kt
+++ src/main/kotlin/io/github/erfangc/assets/SearchAssetController.kt
@@ -1,16 +1,19 @@
 package io.github.erfangc.assets
 
 import io.github.erfangc.assets.models.Asset
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/apis/assets/_search")
+@RequestMapping("/apis/assets")
 class SearchAssetController(private val searchAssetService: SearchAssetService) {
-    @GetMapping
+    @GetMapping("_search")
     fun search(@RequestParam term: String): List<Asset> {
         return searchAssetService.search(term)
+    }
+
+    @PostMapping("_sync-with-dynamo-db")
+    fun syncWithDynamoDB(): String {
+        searchAssetService.syncWithDynamoDB()
+        return "Ok"
     }
 }
